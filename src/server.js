d8bc0ef3326d8122aa4ae5b1c759276654d6fbc8
Thiago Demas <thiago.demas7@gmail.com>
2020-02-22 16:53:45 -0300

    add controllers and dev.js model

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 const server = express();
 
+mongoose.connect('mongodb+srv://admin:admin@cluster0-kkcxs.mongodb.net/omnistack8?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 server.use(express.json());
 server.use(routes);
 
